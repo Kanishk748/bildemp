2198003ad4aa93e8878852562c0519ba92f514ed
Dinesh <dannybrown2710@gmail.com>
2021-06-17 22:36:23 +0530

    Modified changes for kanishk

--- consultancy.js
+++ consultancy.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 let ConsultancySchema = new mongoose.Schema({
-    consultancy_id:String,
-    name :string,
-    website_url:string,
-    mission:string,
-    specializations:string,
-    description:string,
-    audience_id:string,
-    certifications:string,
-    tools:string
+    consultancy_id:{type:String, required:true},
+    name :String,
+    website_url:String,
+    mission:String,
+    specializations:String,
+    description:String,
+    audience_id:String,
+    certifications:String,
+    tools:String
 })
 module.exports = mongoose.model('Consultancy', ConsultancySchema)
